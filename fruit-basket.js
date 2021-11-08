0989092b90a7617cafeaec24334d0c2392c26ee6
amandam2017 <amandamaarman19911@gmail.com>
2021-11-08 16:24:13 +0200

    first commit

---
+++ fruit-basket.js
@@ -0,0 +1,31 @@
+module.exports = function FruitBasket(pool){
+    const dbInstertingQ = async (name, price, quantity) => {
+        // find all the fruit baskets for a given fruit type,
+        const fruits = await pool.query('SELECT fruit_name FROM fruit_basket WHERE fruit_name = $1',[name]);
+
+        if(fruits.rowCount === 0){
+            const count = await pool.query('INSERT INTO fruit_basket (fruit_name, quantity, price) VALUES ($1,$2,$3)', [name, 1, price]);
+        }else{
+            // update the number of fruits in a given basket,
+            var udateBasket = await pool.query('UPDATE fruit_basket SET quantity = quantity+1 WHERE fruit_name = $1', [quantity]);
+        }
+    }
+
+    const getFruitsFromBasketOne = async () => {
+        var allFruits = await pool.query('SELECT fruit_name, quantity, price FROM fruit_basket');
+        return allFruits.rows;
+    } 
+
+      //show the total price of a given fruit basket
+      const getPriceFromBasket = async () => {
+        var totalPrice = await pool.query('SELECT sum(price) FROM fruit_basket');
+        return totalPrice.rows;
+    }
+
+    return{
+        dbInstertingQ,
+        getFruitsFromBasketOne,
+        getPriceFromBasket,
+    }
+}
+
---
+++ test/fuit-basket-tests.js
@@ -0,0 +1,56 @@
+let assert = require("assert");
+let FruitBasket = require("../fruit-basket");
+const pg = require("pg");
+const Pool = pg.Pool;
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/fruit_basket_tests';
+
+const pool = new Pool({
+    connectionString
+});
+
+
+describe('The fruit_basket', function () {
+    beforeEach(async function () {
+        // clean the tables before each test run
+        await pool.query('delete from fruit_basket');
+
+    });
+
+    it('should get fruits from fruit basket', async function () {
+
+        let basketOfFruit = FruitBasket(pool);
+
+        await basketOfFruit.dbInstertingQ('Banana', 3.00)
+        console.log('list?'+ JSON.stringify(await basketOfFruit.getFruitsFromBasketOne()))
+
+    
+        assert.deepEqual( [ {
+            fruit_name: 'Banana', quantity: 1, price: 3.00}], await basketOfFruit.getFruitsFromBasketOne());
+        });
+
+    it('should increament the quantity of fruit that already exist in the fruit basket', async function () {
+
+        let basketOfFruit = FruitBasket(pool);
+
+        await basketOfFruit.dbInstertingQ('Banana', 3.00);
+        await basketOfFruit.dbInstertingQ('Banana', 3.00);
+
+        assert.equal( [ {"quantity": 2,}], await basketOfFruit.getFruitsFromBasketOne());
+
+
+    });
+
+    it('should show the total price for a given fruit basket', async function () {
+
+        let basketOfFruit = FruitBasket(pool);
+
+        await basketOfFruit.dbInstertingQ('Banana', 3.00);
+
+        // console.log('list?'+ JSON.stringify(await basketOfFruit.getFruitsFromBasketOne()))
+
+    
+        assert.equal( [ {"price": 3.00,}], await basketOfFruit.getPriceFromBasket());
+
+    });
+});
